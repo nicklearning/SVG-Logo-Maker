6947480d6d35be44832ada85d9436829dcd7880b
Nick Smith <nick.smith.asu@gmail.com>
2024-01-02 10:34:12 -0700

    complete the prompting

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const inquirer = require('inquirer');
 
+// List of predefined colors
+const predefinedColors = ['red', 'green', 'blue', 'yellow', 'purple', 'white', 'black'];
+
+
 const promptQuestions = [
     {
+
         type: 'input',
         name: 'textLengthInput',
         message: 'Please enter up to three characters:',
@@ -16,13 +21,49 @@ const promptQuestions = [
             return true; // Validation passed
         },
     },
-    /*  {
- 
-     },
-     {
- 
-     } */
-]
+    {
+        type: 'input',
+        name: 'colorInput',
+        message: 'Please enter a text color for your logo. You may enter a standard color or a hexadecimal number color.',
+        validate: colorInput => {
+            // Regular expression to match a hexadecimal color code
+            const hexColorRegex = /^#([0-9a-fA-F]{3}){1,2}$/;
+            const lowerCaseInput = colorInput.toLowerCase();
+
+            if (hexColorRegex.test(lowerCaseInput)) {
+                return true;
+            } else if (predefinedColors.includes(lowerCaseInput)) {
+                return true;
+            } else {
+                return `Please enter a valid hexadecimal color code (e.g., #ff0000) or a valid color from the list: ${predefinedColors.join(', ')}`;
+            }
+        },
+    },
+    {
+        type: 'list',
+        name: 'shapeInput',
+        message: 'Please select a shape color for your logo.',
+        choices: ['circle', 'triangle', 'square'],
+    },
+    {
+        type: 'input',
+        name: 'shapeColorInput',
+        message: 'Please enter a shape color for your logo. You may enter a standard color or a hexadecimal number color.',
+        validate: shapeColorInput => {
+            // Regular expression to match a hexadecimal color code
+            const hexColorRegex = /^#([0-9a-fA-F]{3}){1,2}$/;
+            const lowerCaseInput = shapeColorInput.toLowerCase();
+
+            if (hexColorRegex.test(lowerCaseInput)) {
+                return true;
+            } else if (predefinedColors.includes(lowerCaseInput)) {
+                return true;
+            } else {
+                return `Please enter a valid hexadecimal color code (e.g., #ff0000) or a valid color from the list: ${predefinedColors.join(', ')}`;
+            }
+        },
+    },
+];
 
 inquirer.prompt(promptQuestions)
     // successful Promise will return data to the .then method
